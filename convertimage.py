1e1c6478859524889f281b80eee83df958d9de34
Eduard <yojotony@outlook.com>
2024-04-17 04:07:05 +0300

    feat: integrate tesseract library

--- convertimage.py
+++ convertimage.py
@@ -2,4 +2,13 @@ import cv2
 import pytesseract
 import os
 
-print("Hello!")
+def recognize_letters(image):
+    
+    # Use pytesseract to extract text
+    extracted_text = pytesseract.image_to_string(image, lang='eng', config='-l eng --oem 3 --psm 10')
+    #print(f'Extracted letters: {extracted_text[:1]}')
+
+    # Process extracted text to get 4 letters
+    extracted_letters = extracted_text[:1]
+
+    return extracted_letters

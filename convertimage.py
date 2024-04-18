f7b816cd48666d3cf154424034d1d23791255f43
Eduard <yojotony@outlook.com>
2024-04-18 04:25:14 +0300

    feat: convert image to grayscale and remove background

--- convertimage.py
+++ convertimage.py
@@ -2,6 +2,24 @@ import cv2
 import pytesseract
 import os
 
+def fill_non_white_with_black(image):
+    # Convert image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Threshold the grayscale image to get a binary image where non-white pixels are white
+    _, thresholded = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY_INV)
+
+    # Invert the binary image so non-white pixels become black
+    inverted_thresholded = cv2.bitwise_not(thresholded)
+    
+    # Use the inverted binary image as a mask to fill non-white areas with black
+    result = cv2.bitwise_and(image, image, mask=inverted_thresholded)
+
+    #cv2.imwrite('gray_image.jpg', result)
+
+    return result
+
+# Function to extract 4 letters from a PNG image
 def recognize_letters(image):
     
     # Use pytesseract to extract text
